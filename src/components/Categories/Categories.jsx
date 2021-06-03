6878d0d19b6fbc56626121a327bbceeae0e28fa3
hamellton <hamelltonn@gmail.com>
2021-06-03 19:02:12 +0300

    add categories component and list for him

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 
-const Categories = () => {
+const Categories = ({ items }) => {
   return (
     <div className="categories">
       <ul>
         <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {items.map((el, index) => {
+          return <li key={`${el}__${index}`}>{el}</li>;
+        })}
       </ul>
     </div>
   );
